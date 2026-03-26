5b9b4c06b5026d4e7567225199c56499e7bd8edc
lawrencerosin <lawrencerosin@yahoo.com>
2026-03-26 16:02:05 -0400

    Can add property creation component

--- frontend/src/property creation.ts
+++ frontend/src/property creation.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from "@angular/router";
 @Component({
     selector:"property-creation",
     imports: [Type, RouterOutlet],
-    template:"<input placeholder='Property Name'/><type></type><input placeholder='Property Value'/><router-outlet></router-outlet>"
+    template:"<input placeholder='Property Name'/><type></type><input placeholder='Property Value'/><button>Remove</button><router-outlet></router-outlet>"
 })
 export class PropertyCreation{
 
--- frontend/src/record creation.ts
+++ frontend/src/record creation.ts
@@ -6,17 +6,18 @@ import { RouterOutlet } from "@angular/router";
     imports: [PropertyCreation, RouterOutlet],
     template:`<span id='properties'>
         <span><property-creation></property-creation></span>
-    <button (click)='addProperty()'>Add Property</button></span><router-outlet></router-outlet>`
+    <button (click)='addProperty(adder)' #adder>Add Property</button></span><router-outlet></router-outlet>`
 })
 export class RecordCreation{
-   addProperty(){
+   addProperty(adder:HTMLButtonElement){
    
     const properties:HTMLSpanElement=document.getElementById("properties") as HTMLSpanElement;
+    //The property component
     
-    const adder:HTMLButtonElement=properties.children[properties.children.length-1] as HTMLButtonElement;
     
     if(properties!==null){
-        const newProperty:HTMLSpanElement=document.children[1].cloneNode(true) as HTMLSpanElement;
+        const newProperty:HTMLSpanElement=properties.children[0].cloneNode(true) as HTMLSpanElement;
+        alert("hello");
         properties.insertBefore(newProperty, adder);
     }
    }
