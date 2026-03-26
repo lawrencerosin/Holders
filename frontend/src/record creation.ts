90c88f8ac08fb82efcd0b2623da794fb08f05f63
lawrencerosin <lawrencerosin@yahoo.com>
2026-03-26 15:54:56 -0400

    Fixed record creation

--- frontend/src/record creation.ts
+++ frontend/src/record creation.ts
@@ -4,15 +4,15 @@ import { RouterOutlet } from "@angular/router";
 @Component({
     selector:"record-creation",
     imports: [PropertyCreation, RouterOutlet],
-    template:`<span 
-     >
-        <property-creation></property-creation>
+    template:`<span id='properties'>
+        <span><property-creation></property-creation></span>
     <button (click)='addProperty()'>Add Property</button></span><router-outlet></router-outlet>`
 })
 export class RecordCreation{
    addProperty(){
-    const propertyCreation:HTMLElement=new ViewContainerRef.
+   
     const properties:HTMLSpanElement=document.getElementById("properties") as HTMLSpanElement;
+    
     const adder:HTMLButtonElement=properties.children[properties.children.length-1] as HTMLButtonElement;
     
     if(properties!==null){
